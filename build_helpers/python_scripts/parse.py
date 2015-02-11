9dbd98e71a338ceb28754410fcd463947524f0fa
Joshua Ferdaszewski <ferdaszewski@gmail.com>
2015-02-10 22:38:27 -0800

    Journal page working

--- build_helpers/python_scripts/parse.py
+++ build_helpers/python_scripts/parse.py
@@ -8,12 +8,12 @@ import re
 
 def parse_journal_text(text_file):
     """Parse the text version of the journal entries and return a list
-    of tuples (date, info, journal_entry).
+    of dicts (date, info, journal_entry).
     """
     journal_list = []
     journal_entry = ""
     date = None
-    new_entry = []
+    new_entry = {}
     with codecs.open(text_file, 'r', 'utf_8') as fpt:
 
         # The start of each journal entry, date, and info is on a new line
@@ -21,7 +21,8 @@ def parse_journal_text(text_file):
 
             # The line after the date is the info line
             if date:
-                new_entry.append(line)
+                new_entry['info'] = re.sub(r'\s{2,}', ' ',
+                                           line.replace('\n', ''))
 
                 # Reset date to write journal entries that come after
                 date = None
@@ -31,19 +32,18 @@ def parse_journal_text(text_file):
                                       'aug', 'sep', 'oct', 'nov']:
 
                 # End of the current entry, add journal entry
-                new_entry.append(journal_entry)
-
-                # Remove extra spaces and line brakes
-                new_entry = tuple([re.sub(r'\s{2,}', ' ', im.replace('\n', ''))
-                                  for im in new_entry])
+                new_entry['journal_entry'] = re.sub(
+                    r'\s{2,}', ' ', journal_entry.replace('\n', ''))
 
                 # Don't append the first time as it is empty
-                if new_entry[0]:
+                if len(new_entry) > 1:
                     journal_list.append(new_entry)
 
                 # Reset values to prep for next entry
                 date = line
-                new_entry = [date]
+                new_entry = {}
+                new_entry['date'] = re.sub(r'\s{2,}', ' ',
+                                           date.replace('\n', ''))
                 journal_entry = ""
 
             # The line is a journal entry
@@ -51,11 +51,19 @@ def parse_journal_text(text_file):
                 journal_entry += line
 
         # Write final entry
-        new_entry.append(journal_entry)
-        new_entry = tuple([re.sub(r'\s{2,}', ' ', im.replace('\n', ''))
-                          for im in new_entry])
+        new_entry['journal_entry'] = re.sub(r'\s{2,}', ' ',
+                                            journal_entry.replace('\n', ''))
         journal_list.append(new_entry)
         return journal_list
+
+
+def journal_html(journal):
+    """Takes a list of dicts and returns a rendered HTML page as a
+    string, for use on the new site."""
+    env = jinja2.Environment(
+        loader=jinja2.FileSystemLoader('../jinja_templates'))
+    template = env.get_template('journal_template.html')
+    return template.render(journal)
 
 
 def gen_img_html(num):
@@ -78,8 +86,6 @@ def write_file(text, file_name):
         fpt.write(text)
 
 # Print result of journal text parse
-journal = parse_journal_text('thewalk.txt')
-journal_str = u''
-for entry in journal:
-    journal_str += ('\n'.join(entry) + '\n\n')
-write_file(journal_str, 'journal.tmp')
+journal_list = parse_journal_text('thewalk.txt')
+journal_str = journal_html({'journal': journal_list})
+write_file(journal_str, 'journal.html')
